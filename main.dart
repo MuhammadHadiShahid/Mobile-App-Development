7b8d1867f70a37ca4bb6d40d54320dc9b8cad670
Muhammad Hadi <m.hadishahid7879@gmail.com>
2026-09-03 23:36:23 +0500

    ASSIGNMENT 3

--- main.dart
+++ main.dart
@@ -179,6 +179,23 @@ Map<String, dynamic> person = {
   else {
     print("Product not found");
   }
+
+
+  Map<String, double> expenses = {
+    'sun': 3000.0,
+    'mon': 3000.0,
+    'tue': 3234.0,
+  };
+
+  if 
+  (expenses.containsKey("fri")) {
+    expenses["fri"] = 5000.0;
+  } 
+  else {
+    expenses["fri"] = 5000.0;
+  }
+
+  print(expenses);
 }
 
 

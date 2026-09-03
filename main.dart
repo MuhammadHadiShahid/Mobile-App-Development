6e11662763647be5e840de9aa47cf1abef4a4717
Muhammad Hadi <m.hadishahid7879@gmail.com>
2026-09-03 22:37:11 +0500

    ASSIGNMENT 3

--- main.dart
+++ main.dart
@@ -5,64 +5,114 @@
 
 void main(){
 
-List <String> names = ["Ali" , "Hadi" , "Taha" , "Affan" , "Ahmed"];
+// List <String> names = ["Ali" , "Hadi" , "Taha" , "Affan" , "Ahmed"];
 
-names.forEach((name) {
-    print(name);
-  });
-
-
-List <String>day = [];
-
-day.add("Monday");
-day.add("Tuesday");
-day.add("Wednesday");
-day.add("Thursday");
-day.add("Friday");
-day.add("Saturday");
-day.add("Sunday");
-
-day.forEach ((day) {
-  print(day);
-});
+// names.forEach((name) {
+//     print(name);
+//   });
 
 
- List days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// List <String>day = [];
 
-   print(days);
+// day.add("Monday");
+// day.add("Tuesday");
+// day.add("Wednesday");
+// day.add("Thursday");
+// day.add("Friday");
+// day.add("Saturday");
+// day.add("Sunday");
 
-   days.removeLast();
-   print(days);
-
-   days.removeLast();
-   print(days);
-
-   days.removeLast();
-   print(days);
-
-   days.removeLast();
-   print(days);
-
-   days.removeLast();
-   print(days);
-
-   days.removeLast();
-   print(days);
-
-   days.removeLast();
-   print(days);
+// day.forEach ((day) {
+//   print(day);
+// });
 
 
-List number = [40, 15, 85, 10, 66, 29, 50, 8];
+//  List days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-  int smallest = number.reduce((a, b) => a < b ? a : b);
-  int greatest = number.reduce((a, b) => a > b ? a : b);
+//    print(days);
+
+//    days.removeLast();
+//    print(days);
+
+//    days.removeLast();
+//    print(days);
+
+//    days.removeLast();
+//    print(days);
+
+//    days.removeLast();
+//    print(days);
+
+//    days.removeLast();
+//    print(days);
+
+//    days.removeLast();
+//    print(days);
+
+//    days.removeLast();
+//    print(days);
+
+
+// List number = [40, 15, 85, 10, 66, 29, 50, 8];
+
+//   int smallest = number.reduce((a, b) => a < b ? a : b);
+//   int greatest = number.reduce((a, b) => a > b ? a : b);
 
   
-  print('number: $number');
-  print('Smallest: $smallest');
-  print('Greatest: $greatest');
+//   print('number: $number');
+//   print('Smallest: $smallest');
+//   print('Greatest: $greatest');
+
+
+  // Map<String, String> contacts = {
+  //   "Zain": "123-456-7890",
+  //   "Rayya": "987-654-3210",
+  //   "Hamza": "555-123-4567",
+  //   "Tariq": "444-987-6543",
+  //   "siddique": "111-222-3333",
+  // };
+
+
+  // Iterable<String> keysWithLength4 = contacts.keys.where((key) => key.length == 4);
+
+
+  // print('Keys with length 4: ${keysWithLength4.toList()}');
+
+Map<String, Map<String, Map<String, String>>> world = {
+    "countries": {
+      "USA": {
+        "capitalcity": "washington, D.C.",
+        "currency": "USD",
+        "language": "english",
+      },
+      "Pakistan": {
+        "capitalcity": "Islamabad",
+        "currency": "PKR",
+        "language": "Urdu",
+      },
+      "Japan": {
+        "capitalcity": "Tokyo",
+        "currency": "JPY",
+        "language": "Japanese",
+      },
+    } 
+  };
+ 
+
+ var usaData = world["countries"]?["USA"];
+  print("Capital: ${usaData?['capitalcity']}");
+  print("Currency: ${usaData?['currency']}");
+
+var PakistanData = world["countries"]?["Pakistan"];
+  print("Capital: ${PakistanData?['capitalcity']}");
+  print("Currency: ${PakistanData?['currency']}"); 
+
+var JapanData = world["countries"]?["Japan"];
+  print("Capital: ${JapanData?['capitalcity']}");
+  print("Currency: ${JapanData?['currency']}"); 
+
 }
+
 
 
 
